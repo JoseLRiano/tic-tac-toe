147b357650076e0e0a543488a51f0db2b98c3e15
JoseLRiano <joselriano@yahoo.com>
2019-06-17 13:40:50 -0400

    Updates

---
+++ precache-manifest.106219bfb4ed5447a27e4bac6a9c33ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d35c9261b20e923dc4faa1abc4d43d3e",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "f4a008f76125d4c05461",
+    "url": "/tic-tac-toe/static/css/main.358ebaaa.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/tic-tac-toe/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "f4a008f76125d4c05461",
+    "url": "/tic-tac-toe/static/js/main.feabb35d.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  }
+]);
